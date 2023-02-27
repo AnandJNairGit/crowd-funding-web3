4bd8e27ebf4de70573fdca02cc623af7b187849e
AnandJNairGit <anandjnairit@gmail.com>
2023-02-27 16:24:44 +0530

    create contract context to update and retrive the contract instance

--- src/App.js
+++ src/App.js
@@ -1,31 +1,40 @@
 // import { CssBaseline } from "@mui/material";
-import { BrowserRouter as Router, Routes, Link,Route } from "react-router-dom";
+import { createContext, useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 import NavBar from "./components/navBar";
 import Admin from "./pages/admin";
 import Campaigns from "./pages/campaigns";
 import Home from "./pages/home";
 import MyCampaigns from "./pages/myCampaigns";
+import getContract from "./services/ethers";
 // import Profile from "./pages/Profile";
 
+export const ContractContext = createContext();
 function App() {
+  const [contract, setContract] = useState();
+
+  const updateContract = async () => {
+    setContract(await getContract());
+  };
+  useEffect(() => {
+    updateContract();
+  }, []);
+
   return (
-    
     <>
-
-    {/* <Navbar/> */}
-    <nav>
-      <NavBar/>
-    </nav>
-    <Routes>
-    <Route path="/" element = {<Home/>}/>
-    <Route path="/campaigns" element={<Campaigns/>}/>
-    <Route path="/my-campaigns" element={<MyCampaigns/>}/>
-    <Route path="/admin" element={<Admin/>}/>
-
-
-    {/* <Route path="/Create" element={<Create/>} /> */}
-    {/* <Route path="/Profile" element={<Profile/>} /> */}
-  </Routes>
+      <ContractContext.Provider
+        value={{ contract: contract, updateContract: updateContract }}
+      >
+        <nav>
+          <NavBar />
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/campaigns" element={<Campaigns />} />
+          <Route path="/my-campaigns" element={<MyCampaigns />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </ContractContext.Provider>
     </>
   );
 }
