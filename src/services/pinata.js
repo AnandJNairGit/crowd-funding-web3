72eb9ed875588c7025dc97791744c96721bd92cb
AnandJNairGit <anandjnairit@gmail.com>
2023-02-27 16:27:29 +0530

    add pinata functions for uploading image and metadata to IPFS

---
+++ src/services/pinata.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+const PINATA_API_KEY = process.env.REACT_APP_PINATA_API_KEY;
+const PINATA_API_SECRET = process.env.REACT_APP_PINATA_API_SECRET;
+
+export const uploadImageToIPFS = async (image) => {
+  console.log(process.env);
+  if (image) {
+    try {
+      const formData = new FormData();
+      formData.append("file", image);
+
+      const resFile = await axios({
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        data: formData,
+        headers: {
+          pinata_api_key: `${PINATA_API_KEY}`,
+          pinata_secret_api_key: `${PINATA_API_SECRET}`,
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      console.log("the pinata response is ------------>", resFile);
+
+      const ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
+      console.log(ImgHash);
+      return ImgHash;
+    } catch (error) {
+      console.log("Error sending File to IPFS: ");
+      console.log(error);
+    }
+  }
+};
+
+export const uploadJSONToIPFS = async (jsonData) => {
+  if (jsonData) {
+    try {
+      const res = await axios({
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+        data: JSON.stringify(jsonData),
+        headers: {
+          pinata_api_key: `${PINATA_API_KEY}`,
+          pinata_secret_api_key: `${PINATA_API_SECRET}`,
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("the json response is----------->>>", res);
+      const ipfsUrl = `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`;
+      return ipfsUrl;
+    } catch (error) {
+      console.log("Error sending json to IPFS: ");
+      console.log(error);
+    }
+  }
+};
+
+export const getMetaData = async (ipfsURI) => {
+  let metaData = await axios.get(ipfsURI, {
+    headers: {
+      Accept: "text/plain",
+    },
+  });
+
+  return metaData;
+};
