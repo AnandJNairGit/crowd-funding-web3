642955b001ac70f177bbbb7e1a7d23ebcfb0da3d
AnandJNairGit <anandjnairit@gmail.com>
2023-02-28 18:24:59 +0530

    display campaign list

--- src/pages/campaigns/index.js
+++ src/pages/campaigns/index.js
@@ -1,11 +1,48 @@
-import React from 'react'
-import CreateCampaignRequest from '../../components/createCampaignRequest'
+import { Box } from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+import { ContractContext } from "../../App";
+import BackdropProgress from "../../components/common/BackdropProgress";
+import CreateCampaignRequest from "../../components/createCampaignRequest";
+import { getCampaignsMetadata } from "../../helpers/getCampaignsMetadata";
+import CampaignTile from "./CampaignTile";
 
 const Campaigns = () => {
+  const contract = useContext(ContractContext).contract;
+  const [onGoingCampaigns, setOnGoingCampaigns] = useState();
+
+  const fetchOnGoingCampaigns = async () => {
+    const campaigns = await contract.getOnGoingCampaigns();
+    const campaignsMetadata = await getCampaignsMetadata(campaigns);
+    setOnGoingCampaigns(campaignsMetadata);
+    console.log("the ongoing campaigns are-----------> ", onGoingCampaigns);
+  };
+  useEffect(() => {
+    fetchOnGoingCampaigns();
+
+    return () => {};
+  }, []);
+
   return (
-    <CreateCampaignRequest />
+    <>
+      {onGoingCampaigns ? (
+        <Box
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          {onGoingCampaigns.map((campaign) => (
+            <CampaignTile campaignObj={campaign} />
+          ))}
+        </Box>
+      ) : (
+        <BackdropProgress open={true} />
+      )}
+      <CreateCampaignRequest />
+    </>
+  );
+};
 
-  )
-}
-
-export default Campaigns
+export default Campaigns;
