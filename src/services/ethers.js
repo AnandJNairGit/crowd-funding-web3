88390ae144590815bf411c06844af021ef413cdf
AnandJNairGit <anandjnairit@gmail.com>
2023-03-03 20:15:55 +0530

    return address from ethers service

--- src/services/ethers.js
+++ src/services/ethers.js
@@ -4,8 +4,11 @@ async function getContract(){
     const ethers = require("ethers");
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
+    const accountAddress = await signer.getAddress();
     let contract = new ethers.Contract("0xC53A9a5ab84ed3D1B5196DfA2F7b52d8Cbe6372a", CrowdFunding.abi, signer)
-    return contract;
+    return {accountAddress,contract};
   }
+
+ 
 
   export default getContract;
