fc55830ddc67ee5c33ee29d50d150ce787cca247
AnandJNairGit <anandjnairit@gmail.com>
2023-03-06 19:18:16 +0530

    add try catch block while fetching each campaign

--- src/helpers/getCampaignsMetadata.js
+++ src/helpers/getCampaignsMetadata.js
@@ -5,22 +5,24 @@ export const getCampaignsMetadata = async (campaigns) => {
   try {
     let campaignsInfo = [];
     for (let index = 0; index < campaigns.length; index++) {
-      const campaignMetadata = await (
-        await getMetaData(campaigns[index].ipfsUrl)
-      ).data;
-      const campaignInfo = {
-        id: campaigns[index].id.toNumber(),
-        recepient: campaigns[index].recepient,
-        title: campaignMetadata.campaignTitle,
-        description: campaignMetadata.campaignDescription,
-        requiredFund: campaigns[index].requiredFunding.toNumber(),
-        raisedAmount: campaigns[index].raisedAmount.toNumber(),
-        imageUrl: campaignMetadata.imgUrl,
-        deadline: campaigns[index].deadline.toNumber(),
-        approved: campaigns[index].approved,
-        completed: campaigns[index].completed,
-      };
-      campaignsInfo.push(campaignInfo);
+      try {
+        const campaignMetadata = await (
+          await getMetaData(campaigns[index].ipfsUrl)
+        ).data;
+        const campaignInfo = {
+          id: campaigns[index].id.toNumber(),
+          recepient: campaigns[index].recepient,
+          title: campaignMetadata.campaignTitle,
+          description: campaignMetadata.campaignDescription,
+          requiredFund: campaigns[index].requiredFunding.toNumber(),
+          raisedAmount: campaigns[index].raisedAmount.toNumber(),
+          imageUrl: campaignMetadata.imgUrl,
+          deadline: campaigns[index].deadline.toNumber(),
+          approved: campaigns[index].approved,
+          completed: campaigns[index].completed,
+        };
+        campaignsInfo.push(campaignInfo);
+      } catch (error) {}
     }
     // console.log(campaignsInfo);
     return campaignsInfo;
