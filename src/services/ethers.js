a2d8d6f83825be3ebf02459a62d47ed09b5ab1e6
AnandJNairGit <anandjnairit@gmail.com>
2023-03-06 19:31:38 +0530

    update contract address

--- src/services/ethers.js
+++ src/services/ethers.js
@@ -1,14 +1,16 @@
-import CrowdFunding from "../contractABI/CrowdFunding.json"
+import CrowdFunding from "../contractABI/CrowdFunding.json";
 
-async function getContract(){
-    const ethers = require("ethers");
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const accountAddress = await signer.getAddress();
-    let contract = new ethers.Contract("0xC53A9a5ab84ed3D1B5196DfA2F7b52d8Cbe6372a", CrowdFunding.abi, signer)
-    return {accountAddress,contract};
-  }
+async function getContract() {
+  const ethers = require("ethers");
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const accountAddress = await signer.getAddress();
+  let contract = new ethers.Contract(
+    "0xCA735871a7BE0132A02Df30f0b6110CD279ECe9C",
+    CrowdFunding.abi,
+    signer
+  );
+  return { accountAddress, contract };
+}
 
- 
-
-  export default getContract;
+export default getContract;
