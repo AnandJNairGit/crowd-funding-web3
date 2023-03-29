1e801c08f3f7e969109abe1ae8f82a0eda2a0a9d
AnandJNairGit <anandjnairit@gmail.com>
2023-03-30 04:12:01 +0530

    fix the issue while funding

--- src/services/ethers.js
+++ src/services/ethers.js
@@ -8,7 +8,7 @@ async function getContract() {
 
   // Create the contract instance
   const contract = new ethers.Contract(
-    "0x527E4987b3A82B5de475D28DFB34E1961a0353C2",
+    "0x6f1EACCc7A1C6C49a1043Bb1932C7eCFb0cFd2c1",
     CrowdFunding.abi,
     signer
   );
@@ -20,12 +20,14 @@ async function getContract() {
     minimumContribution,
     allowCrowdFunding,
     allowCampaignCreation,
+    creationPrice
   ] = await Promise.all([
     contract.isAdmin(), // Check if the current user is the admin of the contract
     contract.doesAdminAccountExist(accountAddress), // Check if the current user has admin access
     contract.minimumContribution(), // Get the minimum contribution required to participate in the crowdfunding
     contract.AllowCrowdFunding(), // Check if crowdfunding is allowed for the contract
     contract.AllowCampaignCreation(), // Check if campaign creation is allowed for the contract
+    contract.creationPrice()
   ]);
 
   // Create an object with the constant values returned from the contract methods
@@ -35,6 +37,7 @@ async function getContract() {
     minimumContribution,
     allowCrowdFunding,
     allowCampaignCreation,
+    creationPrice
   };
 
   // Return an object containing the account address, contract instance, and constant values
